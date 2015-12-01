a296f62c20728183262462a30b7614ec4650d471
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-12-01 18:51:24 -0200

    .

---
+++ client/app_cmp.ts
@@ -0,0 +1,13 @@
+import {
+  Component
+} from 'angular2/angular2';
+
+@Component({
+  selector: 'app',
+  template: `
+    <h1>app</h1>
+  `
+})
+export class AppCmp {
+  
+}
---
+++ index.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/angular2';
+import {AppCmp} from 'client/app_cmp.js';
+
+bootstrap(AppCmp);
